224a3c870ce9867f846454d6a4cc525a19433777
Alexander Afanasyev <aa@cs.fiu.edu>
2020-05-30 18:57:07 -0400

    Change download path to `for-ndk21` tag

--- library/github.rb
+++ library/github.rb
@@ -6,5 +6,5 @@ module GitHub
 
   STAGING_SSH_URL       = "git@github.com:#{OWNER}/#{STAGING_REPO_NAME}.git"
   STAGING_HTTPS_URL     = "https://github.com/#{OWNER}/#{STAGING_REPO_NAME}.git"
-  STAGING_DOWNLOAD_BASE = "https://github.com/#{OWNER}/#{STAGING_REPO_NAME}/releases/download/staging"
+  STAGING_DOWNLOAD_BASE = "https://github.com/#{OWNER}/#{STAGING_REPO_NAME}/releases/download/for-ndk21"
 end
